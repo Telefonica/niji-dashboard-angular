3a5c99508fe550b47aecc6841805f3f8ab4cf681
VICTOR MAÑE NUÑEZ <victor.manenunez@telefonica.com>
2019-12-10 09:46:27 +0100

    Added activated route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ActivatedRoute } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ProtectionComponent } from './protection/protection.component';
 
 
 const routes: Routes = [
-  { path: '', component: MainComponent},
-  { path: 'protection', component: ProtectionComponent },
+  { path: '', component: MainComponent, data: { title: 'Dashboard'} },
+  { path: 'protection', component: ProtectionComponent, data: { title: 'Protección de dispositvos'} },
   { path: '**', redirectTo: '' }
 ];
 
