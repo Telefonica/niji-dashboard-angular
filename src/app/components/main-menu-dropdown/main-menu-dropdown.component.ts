1d0260639ad984076e31a37574d33c686d24f442
Víctor Mañé <victor.manenunez@telefonica.com>
2019-12-20 14:49:37 +0100

    New functions and design for menu responsive component

--- src/app/components/main-menu-dropdown/main-menu-dropdown.component.ts
+++ src/app/components/main-menu-dropdown/main-menu-dropdown.component.ts
@@ -7,10 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainMenuDropdownComponent implements OnInit {
   panelOpenState = false;
-
+  public optionHeight = '50px';
+  public cards = [
+    {
+      title: 'Protección Red WiFi',
+      icon: './../../assets/global/img/icons/wifi-icon.svg',
+      expanded: true,
+      contents: [{
+        alias: 'Casa',
+        num: '91 681 74 17',
+        link: 'wifi-protection'
+      }],
+    },
+    {
+      title: 'Protección Red Móvil',
+      icon: './../../assets/global/img/icons/smartphone-icon.svg',
+      expanded: false,
+      contents: [
+        {
+          alias: 'Julia',
+          num: '634 567 879',
+          link: 'otherlink'
+        },
+        {
+          alias: 'Daniel',
+          num: '678 123 456',
+          link: 'otherlink'
+        },
+        {
+          alias: 'Iratxe',
+          num: '654 321 543',
+          link: 'otherlink'
+        },
+      ],
+    },
+    {
+      title: 'Consejos navegación segura',
+      icon: './../../assets/global/img/icons/heart-icon.svg',
+      expanded: false,
+      link: 'otherlink'
+    },
+    {
+      title: 'Darse de baja',
+      icon: './../../assets/global/img/icons/alert-icon.svg',
+      link: 'otherlink'
+    },
+    {
+      title: 'Cerrar sesión',
+      icon: './../../assets/global/img/icons/log-out-icon.svg',
+      link: 'otherlink'
+    }
+  ];
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
