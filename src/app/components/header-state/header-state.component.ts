a75e9ad00054c52c4388fc3586c1a2b89e8cd8e3
Víctor Mañé <victor.manenunez@telefonica.com>
2019-12-31 10:04:59 +0100

    Security status header

--- src/app/components/header-state/header-state.component.ts
+++ src/app/components/header-state/header-state.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header-state',
   templateUrl: './header-state.component.html',
-  styleUrls: ['./header-state.component.css']
+  styleUrls: ['./header-state.component.scss']
 })
+
 export class HeaderStateComponent implements OnInit {
+  public warning = true;
+  public HEADER_TEXT: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.warning
+      ? this.HEADER_TEXT = 'Activa la Protección Web en todas tus líneas'
+      : this.HEADER_TEXT = 'Todo en calma, estás protegido';
   }
 
 }
