fc71d75aaf8b48e71995c78bacc3cab298423286
Víctor Mañé <victor.manenunez@telefonica.com>
2020-01-07 11:40:52 +0100

    security level component

--- src/app/components/security-level/security-level.component.ts
+++ src/app/components/security-level/security-level.component.ts
@@ -1,26 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-security-level',
-    templateUrl: './security-level.component.html',
-    styleUrls: ['./security-level.component.scss']
+  selector: 'app-security-level',
+  templateUrl: './security-level.component.html',
+  styleUrls: ['./security-level.component.scss']
 })
 export class SecurityLevelComponent implements OnInit {
-    public SECURITY_LEVEL_LOW = 2;
-    public SECURITY_LEVEL_MEDIUM = 5;
-    public SECURITY_LEVEL_HIGH = 8;
-
-    public securityLevel = [
-        {
-            levelCaption: 'NIVEL DE PROTECCIÓN:',
-            protectionCaption: 'BAJA',
-            link: 'Aumenta el nivel de protección',
-            protection: 1
-        }
-    ];
-    constructor() { }
-
-    ngOnInit() {
+  public SECURITY_LEVEL_LOW = 2;
+  public SECURITY_LEVEL_MEDIUM = 5;
+  public SECURITY_LEVEL_HIGH = 8;
+  public open = true;
+  public warningStatusMsg = 'Te recomendamos revisar este perfil, no tiene la protección básica de tu red móvil activada. Y puede sufrir amenazas';
+  public securityLevel = [
+    {
+      levelCaption: 'NIVEL DE PROTECCIÓN:',
+      protectionCaption: 'BAJA',
+      link: 'Aumenta el nivel de protección',
+      protection: 1
     }
+  ];
+  constructor() { }
 
+  ngOnInit() {
+  }
+  close() {
+    this.open = false;
+  }
 }
--- src/app/pages/protection/protection.component.ts
+++ src/app/pages/protection/protection.component.ts
@@ -1,94 +1,91 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 @Component({
-    selector: 'app-protection',
-    templateUrl: './protection.component.html',
-    styleUrls: ['./protection.component.scss']
+  selector: 'app-protection',
+  templateUrl: './protection.component.html',
+  styleUrls: ['./protection.component.scss']
 })
 export class ProtectionComponent implements OnInit {
-    public header = [
+  public header = [
+    {
+      title: 'Casa',
+      num: '916 817 417'
+    }
+  ];
+  public cards = [
+    {
+      title: 'Protección WiFi Hogar',
+      subtitleLlink: 'Descubre más',
+      showCheck: true,
+      icon: './../../assets/global/img/icons/shield-icon.svg',
+      checked: true,
+      disabled: false,
+
+      states: [
+
+        // samples of states
         {
-            title: 'Casa',
-            num: '916 817 417'
+          // unprotected: {
+          //   title: '¡Estás desprotegido!',
+          //   icon: './../../assets/global/img/icons/no-shield-tick-icon.svg',
+          //   label: 'Protección desactivada'
+          // },
+          undetected: {
+            title: '¡Sin amenazas detectadas!',
+            icon: './../../assets/global/img/icons/no-threats-detected-icon.svg',
+            label: 'Tu navegación ha sido excelente'
+          },
+          // detected: {
+          //   title: '30/04/2019 - 25/05/2019',
+          //   total: '114',
+          //   label: 'Amenazas bloqueadas'
+          // }
         }
-    ];
-    public cards = [
+      ],
+      contents: [
         {
-            title: 'Protección WiFi Hogar',
-            subtitleLlink: 'Descubre más',
-            showCheck: true,
-            icon: './../../assets/global/img/icons/shield-icon.svg',
-            checked: true,
-            disabled: false,
-
-            states: [
-
-                // samples of states
-                {
-                    // unprotected: {
-                    //   title: '¡Estás desprotegido!',
-                    //   icon: './../../assets/global/img/icons/no-shield-tick-icon.svg',
-                    //   label: 'Protección desactivada'
-                    // },
-                    undetected: {
-                        title: '¡Sin amenazas detectadas!',
-                        icon: './../../assets/global/img/icons/no-threats-detected-icon.svg',
-                        label: 'Tu navegación ha sido excelente'
-                    },
-                    // detected: {
-                    //   title: '30/04/2019 - 25/05/2019',
-                    //   total: '114',
-                    //   label: 'Amenazas bloqueadas'
-                    // }
-                }
-            ],
-            contents: [
-                {
-                    linkTitle: 'Historial de amenazas',
-                    link: '/threats-history'
-                },
-                {
-                    linkTitle: 'Páginas de confianza',
-                    description: 'Añade a tu lista tus páginas de confianza y gestiónalas',
-                    link: '/trusted-pages'
-                },
-            ]
+          linkTitle: 'Historial de amenazas',
+          link: '/threats-history'
         },
         {
-            title: 'Bloqueo de contenido',
-            subtitle: 'Filtrado de sitios web apropiados para la edad',
-            subtitleLlink: 'Activar en Smart Wifi',
-            showCheck: true,
-            disabled: false,
-            checked: true,
-            icon: './../../assets/global/img/icons/block-icon.svg',
+          linkTitle: 'Páginas de confianza',
+          description: 'Añade a tu lista tus páginas de confianza y gestiónalas',
+          link: '/trusted-pages'
         },
-        {
-            title: 'Protección contra rastreo',
-            subtitle: 'Bloquea los rastreadores de tu publicidad en tu navegador',
-            subtitleLlink: 'Descubre más',
-            showCheck: true,
-            icon: './../../assets/global/img/icons/lens-glass-icon.svg',
-            checked: true,
-            disabled: false
-        },
-        {
-            title: 'Seguridad Dispositivo',
-            subtitleLlink: 'Descubre más',
-            showCheck: true,
-            icon: './../../assets/global/img/icons/lock-icon.svg',
-            checked: true,
-            disabled: false,
-            cardDescription: 'Protege 5 dispositivos a tu elección, tanto dentro como fuera de casa.',
-        },
-    ];
-    public title: any;
+      ]
+    },
+    {
+      title: 'Bloqueo de contenido',
+      subtitle: 'Filtrado de sitios web apropiados para la edad',
+      subtitleLlink: 'Activar en Smart Wifi',
+      showCheck: true,
+      disabled: false,
+      checked: true,
+      icon: './../../assets/global/img/icons/block-icon.svg',
+    },
+    {
+      title: 'Protección contra rastreo',
+      subtitle: 'Bloquea los rastreadores de tu publicidad en tu navegador',
+      subtitleLlink: 'Descubre más',
+      showCheck: true,
+      icon: './../../assets/global/img/icons/lens-glass-icon.svg',
+      checked: true,
+      disabled: false
+    },
+    {
+      title: 'Seguridad Dispositivo',
+      subtitleLlink: 'Descubre más',
+      icon: './../../assets/global/img/icons/lock-icon.svg',
+      cardDescription: 'Protege 5 dispositivos a tu elección, tanto dentro como fuera de casa.',
+    },
+  ];
+  public title: any;
 
-    constructor() {
-    }
+  constructor() {
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
 
 }
 
