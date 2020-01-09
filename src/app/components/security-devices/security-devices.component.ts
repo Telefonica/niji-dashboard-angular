4e2576892205efd0a6db0274b3fc40d742147409
Víctor Mañé <victor.manenunez@telefonica.com>
2020-01-09 10:49:42 +0100

    Protected devices list

--- src/app/components/security-devices/security-devices.component.ts
+++ src/app/components/security-devices/security-devices.component.ts
@@ -8,11 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class SecurityDevicesComponent implements OnInit {
   public card = [
     {
+      cardTitle: 'Mis dispositivos protegidos',
       cardDescription: 'Protege 5 dispositivos a tu elección, tanto dentro como fuera de casa.',
       btn: 'Descargar licencias',
       licensesCaption: 'Licencias Disponibles',
       dimiss: 'Dar de baja Seguridad Dispositivo',
-      level : 3
+      level: 3,
+      protectedDevices: [
+        {
+          name: 'iPhone 8',
+          link: 'sampleLink'
+        },
+        {
+          name: 'Samsung S10'
+        },
+        {
+          name: 'Xiaomi Redmi Note 7'
+        }
+      ]
     }
   ]
   constructor() { }
